e07ec30c314e3f16eae0f9e015e54a49c06182c6
rahmaboukhris <64844430+rahmaboukhris@users.noreply.github.com>
2020-05-12 23:09:41 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -7,9 +7,7 @@ int main(int argc, char* argv[]) {
 
   if (num == 0) {
   	cout << "No arguments provided\n";
-  } else if (num == 0) { // intentional mistake
-  	cout << "1 argument provided\n";
-  } else if (num == 2) {
+  }  else if (num == 2) {
   	cout << "2 arguments provided\n";
   } else {
   	cout << num << " arguments provided\n";
